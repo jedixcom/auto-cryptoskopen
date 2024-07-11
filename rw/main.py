575cd4514db1e675bc1c5ce6f266a787b3ad7aef
_akira <jedi@xcom.dev>
2024-07-11 03:11:51 +0200

    23

--- rw/main.py
+++ rw/main.py
@@ -22,7 +22,7 @@ load_dotenv()
 bucket_name = os.getenv('BUCKET_NAME')
 base_dir = os.getenv('BASE_DIR')
 google_application_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-domain_name = os.getenv('DOMAIN_NAME', 'https://auto-cryptoskopen-1.web.app/')
+domain_name = os.getenv('DOMAIN_NAME')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 mongo_uri = os.getenv('MONGO_URI')
 
